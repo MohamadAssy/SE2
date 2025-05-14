29b7385a1b193dbd1f1c5e25fab988b624d73664
Mohamad Assy <mhmd8assy@gmail.com>
2025-05-14 12:57:58 +0300

    Add updated models for review

--- src/model/Book.model.ts
+++ src/model/Book.model.ts
@@ -1,36 +1,56 @@
+
 export class Book {
-    private title: string;
-    private author: string;
-    private isbn: string; // international standard book number
-    private pages: number;
-  
-    constructor(
-        title: string,
-        author: string, 
-        isbn: string, 
-        pages: number
-    ) 
-    {
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
-      this.pages = pages;
-    }
-  
-    getTitle(): string {
-      return this.title;
-    }
-  
-    getAuthor(): string {
-      return this.author;
-    }
-  
-    getIsbn(): string {
-      return this.isbn;
-    }
-  
-    getPages(): number {
-      return this.pages;
-    }
+  private title: string;
+  private author: string;
+  private genre: string;
+  private format: string;
+  private language: string;
+  private publisher: string;
+  private specialEdition: string;
+  private packaging: string;
+
+  constructor(
+    title: string,
+    author: string,
+    genre: string,
+    format: string,
+    language: string,
+    publisher: string,
+    specialEdition: string,
+    packaging: string
+  ) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.format = format;
+    this.language = language;
+    this.publisher = publisher;
+    this.specialEdition = specialEdition;
+    this.packaging = packaging;
   }
-  
+
+  getTitle(): string {
+    return this.title;
+  }
+  getAuthor(): string {
+    return this.author;
+  }
+  getGenre(): string {
+    return this.genre;
+  }
+  getFormat(): string {
+    return this.format;
+  }
+  getLanguage(): string {
+    return this.language;
+  }
+  getPublisher(): string {
+    return this.publisher;
+  }
+  getSpecialEdition(): string {
+    return this.specialEdition;
+  }
+  getPackaging(): string {
+    return this.packaging;
+  }
+}
--- src/model/Toy.model.ts
+++ src/model/Toy.model.ts
@@ -1,36 +1,44 @@
+
 export class Toy {
-    private name: string;
-    private type: string;
-    private price: number;
-    private ageGroup: string;
-  
-    constructor(
-        name: string,
-        type: string,
-        price: number,
-        ageGroup: string
-    ) 
-    {
-      this.name = name;
-      this.type = type;
-      this.price = price;
-      this.ageGroup = ageGroup;
-    }
-  
-    getName(): string {
-      return this.name;
-    }
-  
-    getType(): string {
-      return this.type;
-    }
-  
-    getPrice(): number {
-      return this.price;
-    }
-  
-    getAgeGroup(): string {
-      return this.ageGroup;
-    }
+  private type: string;
+  private ageGroup: string;
+  private brand: string;
+  private material: string;
+  private batteryRequired: string;
+  private educational: string;
+
+  constructor(
+    type: string,
+    ageGroup: string,
+    brand: string,
+    material: string,
+    batteryRequired: string,
+    educational: string
+  ) {
+    this.type = type;
+    this.ageGroup = ageGroup;
+    this.brand = brand;
+    this.material = material;
+    this.batteryRequired = batteryRequired;
+    this.educational = educational;
   }
-  
+
+  getType(): string {
+    return this.type;
+  }
+  getAgeGroup(): string {
+    return this.ageGroup;
+  }
+  getBrand(): string {
+    return this.brand;
+  }
+  getMaterial(): string {
+    return this.material;
+  }
+  getBatteryRequired(): string {
+    return this.batteryRequired;
+  }
+  getEducational(): string {
+    return this.educational;
+  }
+}
